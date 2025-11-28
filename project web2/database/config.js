f918106490fc8139a25e928ae241523a25967375
ZakaMD-lgtm <investasicrypto135@gmail.com>
2025-11-29 03:24:07 +0700

    Upload project web2/database/config.js

---
+++ project web2/database/config.js
@@ -0,0 +1,6 @@
+// ©AiiSigma
+
+module.exports = {
+  domain: "https://panel.dalangstore.my.id", // Ganti sma domain panel lu dan pastiin harus *http*
+  port: "7120" // Ganti Portnoy di panel network.
+};
